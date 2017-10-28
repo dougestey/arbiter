10410496a8a18224869a07b4bcd7cff04e230fe9
Doug Estey <doug.estey@gmail.com>
2017-10-28 00:50:14 -0700

    eve-swagger -> eve-swagger-simple

--- api/services/Swagger.js
+++ api/services/Swagger.js
@@ -1,25 +1,15 @@
-// The main module returns a default Api instance with an attached
-// Api constructor if configuration changes are necessary.
-let esi = require('eve-swagger');
-
-// Creating a new Api instance with a different configuration.
-// All options, with their default values, are shown below.
-let esi2 = esi({
-    service: 'https://esi.tech.ccp.is',
-    source: 'tranquility',
-    agent: 'ascent | https://github.com/dougestey/ascent',
-    language: 'en-us',
-    timeout: 6000,
-    minTime: 0,
-    maxConcurrent: 0
-  });
+// Originally we used eve-swagger but had some weird issues
+// coming up in stats (possibly due to old endpoints.)
+let esi = require('eve-swagger-simple');
 
 module.exports = {
 
   initialize: function() {
     sails.log.debug('[Swagger.initialize] Running');
-    return esi.solarSystems.all()
-      .then((systems) => {
+    return esi.request('/universe/systems', {
+      datasource: 'tranquility',
+    })
+    .then((systems) => {
         sails.log.debug('[Swagger.initialize] Got response from ESI, ' + systems.length + ' total.');
 
         let fn = function(system) {
@@ -48,36 +38,40 @@ module.exports = {
 
   updateKills: function() {
     sails.log.debug('[Swagger.updateKills] Running');
-    return esi.solarSystems.killStats()
-      .then((systems) => {
-        sails.log.debug('[Swagger.updateKills] Got response from ESI, ' + systems.length + ' total.');
+    return esi.request('/universe/system_kills', {
+      datasource: 'tranquility',
+    })
+    .then((systems) => {
+      sails.log.debug('[Swagger.updateKills] Got response from ESI, ' + systems.length + ' total.');
 
-        let fn = function(system) {
-          sails.log.debug('[Swagger.updateKills] Updating: ' + system.system_id);
-          return System.update({ systemId: system.system_id }, {
-            shipKills: system.ship_kills,
-            npcKills: system.npc_kills,
-            podKills: system.pod_kills
-          }).then((updated) => {
-            return updated;
-          }, (error) => {
-            sails.log.error(error);
-          });
-        };
+      let fn = function(system) {
+        sails.log.debug('[Swagger.updateKills] Updating: ', system);
+        return System.update({ systemId: system.system_id }, {
+          shipKills: system.ship_kills,
+          npcKills: system.npc_kills,
+          podKills: system.pod_kills
+        }).then((updated) => {
+          return updated;
+        }, (error) => {
+          sails.log.error(error);
+        });
+      };
 
-        let resolvedSystems = systems.map(fn);
+      let resolvedSystems = systems.map(fn);
 
-        return Promise.all(resolvedSystems)
-          .then((systems) => {
-            sails.log.debug('[Swagger.updateKills] Done. Returned ' + systems.length);
-            return systems;
-          });
-      });
+      return Promise.all(resolvedSystems)
+        .then((systems) => {
+          sails.log.debug('[Swagger.updateKills] Done. Returned ' + systems.length);
+          return systems;
+        });
+    });
   },
 
   updateJumps: function() {
     sails.log.debug('[Swagger.updateJumps] Running');
-    return esi.solarSystems.jumpStats()
+    return esi.request('/universe/system_jumps', {
+      datasource: 'tranquility',
+    })
       .then((systems) => {
         sails.log.debug('[Swagger.updateJumps] Got response from ESI, ' + systems.length + ' total.');
 
