080a93d9a2f5d90cc9da2302bd903e267aa23051
Doug Estey <doug.estey@gmail.com>
2018-02-03 20:34:40 -0500

    fix bugs in character update for new chars. add jobs for system stats

--- api/services/Scheduler.js
+++ api/services/Scheduler.js
@@ -9,6 +9,28 @@ module.exports = {
     });
 
     job.save();
+  },
+
+  updateKills() {
+    let job = sails.config.jobs.create('update_kills');
+
+    job.on('failed', function(err) {
+      console.error('[Scheduler.updateKills] Job failed');
+      console.error(err);
+    });
+
+    job.save();
+  },
+
+  updateJumps() {
+    let job = sails.config.jobs.create('update_jumps');
+
+    job.on('failed', function(err) {
+      console.error('[Scheduler.updateJumps] Job failed');
+      console.error(err);
+    });
+
+    job.save();
   }
 
 };
