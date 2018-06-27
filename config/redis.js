aed9a5f0492add9f638fb05c89e8a9f2c64a6ea0
Doug Estey <doug.estey@gmail.com>
2018-06-26 20:12:46 -0400

    cleanup

--- config/redis.js
+++ config/redis.js
@@ -4,4 +4,7 @@ var redisUrl = url.parse('redis://user:@localhost:6379/');
 
 var db = redis.createClient();
 
+if (process.env.FLUSH_REDIS === 'true')
+  db.flushdb();
+
 module.exports.redis = db;
--- jobs/SwaggerUpdates.js
+++ jobs/SwaggerUpdates.js
@@ -1,12 +1,12 @@
-const everyFiveSeconds = 10000;
+const everyTenSeconds = 10000;
 const everyOneHour = 3600000;
 
 let SwaggerUpdates = {
 
   kickoff() {
-    setInterval(ActiveSockets.scheduleUpdatesForActiveSockets, everyFiveSeconds);
+    setInterval(ActiveSockets.scheduleUpdatesForActiveSockets, everyTenSeconds);
     setInterval(Scheduler.updateStats, everyOneHour); // TODO: Read headers to optimize call time
-    Scheduler.updateStats();
+    setTimeout(Scheduler.updateStats, 300000);
   }
 
 };
