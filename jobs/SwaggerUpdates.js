8b98d23357177f5a4f79a13dc66b3f5a2beb88d5
Doug Estey <doug.estey@gmail.com>
2018-07-13 18:21:03 -0400

    only schedule stat updates in production

--- jobs/SwaggerUpdates.js
+++ jobs/SwaggerUpdates.js
@@ -4,11 +4,14 @@ const everyOneHour = 3600000;
 let SwaggerUpdates = {
 
   kickoff() {
+    // Connected character location intervals
     setInterval(ActiveSockets.scheduleUpdatesForActiveSockets, everyTenSeconds);
-    setInterval(Scheduler.updateStats, everyOneHour); // TODO: Read headers to optimize call time
 
-    // Settle down before doing heavy processing
-    setTimeout(Scheduler.updateStats, 300000);
+    // System, constellation and region stats
+    if (process.env.NODE_ENV === 'production') {
+      setInterval(Scheduler.updateStats, everyOneHour); // TODO: Read headers to optimize call time
+      setTimeout(Scheduler.updateStats, 300000); // First kick
+    }
   }
 
 };
